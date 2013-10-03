b8b4a9ef2c8b4155abedf779947a67f98af369cd
Mark Van Moer <mvanmoer@illinois.edu>
2013-10-03 16:15:26 -0500

    added thermal derived type ->
    This is a derived type to handle different thermal initial conditions. Ideally this would probably have a method for creating an arbitrary number of thermals, checking that they don't overlap, etc. But for now just wanted to be able to read in thermal settings from a config file.

--- PGM5/E/initial_conditions.f90
+++ PGM5/E/initial_conditions.f90
@@ -6,6 +6,12 @@
 module initial_conditions
   implicit none
   public    
+  type thermal
+    real :: x, y, z
+    real :: rx, ry, rz
+    real :: dtPrime
+    real :: dU, dV, dW
+  end type thermal
 contains  
   subroutine ic(t, u, v, rho_t, rho_w, nx, ny, nz, dx, dy, dz)
 
@@ -23,15 +29,7 @@ contains
     real, intent(in) :: dx, dy, dz
     integer, intent(in) :: nx, ny, nz
  
-    real, parameter :: x1_0 =   250.0, y1_0 = 8250.0, z1_0 = 4250.0
-    real, parameter :: x2_0 = 16250.0, y2_0 = 8250.0, z2_0 = 4250.0
-    real, parameter :: xr1 = 4000.0, yr1 = 999999.0, zr1 = 4000.0
-    real, parameter :: xr2 = 4000.0, yr2 = 999999.0, zr2 = 4000.0
-    real, parameter :: dtPrime1 = -20.0
-    real, parameter :: dtPrime2 = -20.0
     real, parameter :: Thetabar = 300.0
-    real, parameter :: deltaV1 = -25.0
-    real, parameter :: deltaV2 = 25.0
     real, parameter :: g = 9.81
     real, parameter :: c_p = 1004.0
     real, parameter :: R_d = 287.0
@@ -43,6 +41,24 @@ contains
     real :: ranval, rand
     integer :: i, j, k
     real :: r1, r2, x, y, z
+
+    type (thermal) t1, t2
+    t1%x = 250.0
+    t1%y = 8250.0
+    t1%z = 4250.0
+    t2%x = 16250.0
+    t2%y = 8250.0
+    t2%z = 4250.0
+    t1%rx = 4000.0
+    t1%ry = 999999.0
+    t1%rz = 4000.0
+    t2%rx = 4000.0
+    t2%ry = 999999.0
+    t2%rz = 4000.0
+    t1%dtPrime = -20.0
+    t2%dtPrime = -20.0
+    t1%dV = -25.0
+    t2%dV = 25.0
 
     pi = 4.0 * atan(1.0)
 
@@ -81,27 +97,25 @@ contains
              x = real(i - 1) * dx + dx / 2.0
              y = real(j - 1) * dy + dy / 2.0
              z = real(k - 1) * dz + dz / 2.0
-             
-             ! radius only one perturbation for Tests A,B,C
-             r1 = sqrt(((x - x1_0)/xr1)**2 + ((y - y1_0)/yr1)**2 + &
-                  ((z - z1_0)/zr1)**2)
-             r2 = sqrt(((x - x2_0)/xr2)**2 + ((y - y2_0)/yr2)**2 + &
-                  ((z - z2_0)/zr2)**2)
-                          
+                         
+             r1 = sqrt(((x - t1%x)/t1%rx)**2 + &
+                       ((y - t1%y)/t1%ry)**2 + &
+                       ((z - t1%z)/t1%rz)**2)
+
+             r2 = sqrt(((x - t2%x)/t2%rx)**2 + &
+                       ((y - t2%y)/t2%ry)**2 + &
+                       ((z - t2%z)/t2%rz)**2)
              
              if (r1 <= 1.0) then 
-                t(i,j,k) = t(i,j,k) + (dtPrime1 * (cos(r1 * pi) + 1.0)/2.0)
-                v(i,j,k) = v(i,j,k) + (deltaV1 * (cos(r1 * pi) + 1.0)/2.0)
+                t(i,j,k) = t(i,j,k) + (t1%dtPrime * (cos(r1 * pi) + 1.0)/2.0)
+                v(i,j,k) = v(i,j,k) + (t1%dV * (cos(r1 * pi) + 1.0)/2.0)
               else if (r2 <= 1.0) then
-                 t(i,j,k) = t(i,j,k) + (dtPrime2 * (cos(r2 * pi) + 1.0)/2.0)
-                 v(i,j,k) = v(i,j,k) + (deltaV2 * (cos(r2 * pi) + 1.0)/2.0)
+                 t(i,j,k) = t(i,j,k) + (t2%dtPrime * (cos(r2 * pi) + 1.0)/2.0)
+                 v(i,j,k) = v(i,j,k) + (t2%dV * (cos(r2 * pi) + 1.0)/2.0)
              end if
              
           enddo
        enddo
     enddo
-
-
- 
   end subroutine ic
 end module initial_conditions
