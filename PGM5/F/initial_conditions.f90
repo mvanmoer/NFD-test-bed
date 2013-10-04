2cb9dfb46b4489889c439051221c2e4207831c85
Mark Van Moer <mvanmoer@illinois.edu>
2013-10-04 13:48:29 -0500

    from E

--- PGM5/F/initial_conditions.f90
+++ PGM5/F/initial_conditions.f90
@@ -2,14 +2,19 @@
 !  Mark Van Moer, ATMS502 Fall 2011
 !  Sets the initial conditions.
 !  First set the "base state vertical profiles"
-!  
-!  Test F
-!
+
 module initial_conditions
+  use mesh_type
   implicit none
   public    
+  type thermal
+    real :: x, y, z
+    real :: rx, ry, rz
+    real :: dtPrime
+    real :: dU, dV, dW
+  end type thermal
 contains  
-  subroutine ic(t, u, v, rho_t, rho_w, nx, ny, nz, dx, dy, dz)
+  subroutine ic(t, u, v, rho_t, rho_w, m)
 
     ! t -- "theta", 3D array of scalars
     ! u -- 3D array of momentum u-component
@@ -22,37 +27,46 @@ contains
     real, dimension(:,:,:), intent(inout) :: t, u, v
     real, dimension(:), intent(inout) :: rho_t, rho_w
     real :: pi
-    real, intent(in) :: dx, dy, dz
-    integer, intent(in) :: nx, ny, nz
+    type (mesh), intent(in) :: m
+    integer :: numthermals
  
-    real, parameter :: x1_0 =   250.0, y1_0 = 10750.0, z1_0 = 1250.0
-    real, parameter :: x2_0 = 16250.0, y2_0 = 8250.0, z2_0 = 4250.0
-    real, parameter :: xr1 = 4000.0, yr1 = 999999.0, zr1 = 4000.0
-    real, parameter :: xr2 = 4000.0, yr2 = 999999.0, zr2 = 4000.0
-    real, parameter :: dtPrime1 = -10.0
-    real, parameter :: dtPrime2 = 0.0
     real, parameter :: Thetabar = 300.0
-    real, parameter :: deltaV1 = -25.0
-    real, parameter :: deltaV2 = 0.0
     real, parameter :: g = 9.81
     real, parameter :: c_p = 1004.0
     real, parameter :: R_d = 287.0
     real, parameter :: P_0 = 100000.0
-    real, parameter :: uperturb = 10.0
-    real, dimension(nz) :: zed, Tbar, Pbar
+    real, parameter :: uperturb = 1.0
+    real, dimension(m%nz) :: zed, Tbar, Pbar
+    real, parameter :: flag = 9999.0
 
     real :: ranval, rand
-    integer :: i, j, k
-    real :: r1, r2, x, y, z
+    integer :: i, j, k, ii
+    real :: d, x, y, z
 
+    type (thermal), allocatable, dimension(:) :: therms
+    integer, parameter :: iounit = 11
+
+    ! read thermal data from input.. feel like this should be wrapped, somehow
+    open(unit=iounit,file='thermals.input',status='old')
+    rewind(iounit)
+    read(iounit,*) numthermals
+    allocate(therms(numthermals))    
+
+    do i = 1, numthermals
+      read(iounit,*) therms(i)%x, therms(i)%y, therms(i)%z, &
+                     therms(i)%rx, therms(i)%ry, therms(i)%rz, &
+                     therms(i)%dtPrime, therms(i)%dU, therms(i)%dV, therms(i)%dW
+    end do
+    close (iounit)
+    
     pi = 4.0 * atan(1.0)
 
     ! base state vertical profiles
     ! each z-slice is the same
    
-    do k = 1, nz
+    do k = 1, m%nz
        ! height in meters at Theta,u,pPrime levels
-       zed(k) = dz / 2.0 + dz * real(k - 1)
+       zed(k) = m%dz * 0.5 + m%dz * real(k - 1)
 
        ! temperature in Kelvins 
        Tbar(k) = Thetabar - g/c_p * zed(k)
@@ -65,54 +79,38 @@ contains
     end do
 
     ! rho_w is staggered from rho_t
-    do k = 2, nz
+    do k = 2, m%nz
        rho_w(k) = 0.5 * (rho_t(k) + rho_t(k-1))
     end do
-    rho_w(1) = 9999.0       ! flag -- unused
-    rho_w(nz+1) = 9999.0    ! flag -- unused
+    rho_w(1) = flag
+    rho_w(m%nz+1) = flag
        
     ! Loop over physical domain.
     t = Thetabar
     v = 0.0
-    do k = 1, nz    
-       do j = 1, ny
-          do i = 1, nx
+    
+    do ii = 1, numthermals 
+    do k = 1, m%nz    
+       do j = 1, m%ny
+          do i = 1, m%nx
 
              ! physical coordinate generation
-             x = real(i - 1) * dx + dx / 2.0
-             y = real(j - 1) * dy + dy / 2.0
-             z = real(k - 1) * dz + dz / 2.0
+             x = real(i - 1) * m%dx + m%dx * 0.5
+             y = real(j - 1) * m%dy + m%dy * 0.5
+             z = real(k - 1) * m%dz + m%dz * 0.5
+                        
+             d = sqrt(((x - therms(ii)%x)/therms(ii)%rx)**2 + &
+                       ((y - therms(ii)%y)/therms(ii)%ry)**2 + &
+                       ((z - therms(ii)%z)/therms(ii)%rz)**2)
              
-             ! radius only one perturbation for Tests A,B,C
-             r1 = sqrt(((x - x1_0)/xr1)**2 + ((y - y1_0)/yr1)**2 + &
-                  ((z - z1_0)/zr1)**2)
-             r2 = sqrt(((x - x2_0)/xr2)**2 + ((y - y2_0)/yr2)**2 + &
-                  ((z - z2_0)/zr2)**2)
-                          
-             
-             if (r1 <= 1.0) then 
-                t(i,j,k) = t(i,j,k) + (dtPrime1 * (cos(r1 * pi) + 1.0)/2.0)
-                v(i,j,k) = v(i,j,k) + (deltaV1 * (cos(r1 * pi) + 1.0)/2.0)
-              else if (r2 <= 1.0) then
-                 t(i,j,k) = t(i,j,k) + (dtPrime2 * (cos(r2 * pi) + 1.0)/2.0)
-                 v(i,j,k) = v(i,j,k) + (deltaV2 * (cos(r2 * pi) + 1.0)/2.0)
+             if (d <= 1.0) then 
+                t(i,j,k) = t(i,j,k) + (therms(ii)%dtPrime * (cos(d * pi) + 1.0)*0.5)
+                v(i,j,k) = v(i,j,k) + (therms(ii)%dV * (cos(d * pi) + 1.0)*0.5)
              end if
              
           enddo
        enddo
     enddo
-
-    ! random U perturbations
-    call srand(0)
-    do k = 1, nz
-        do j = 1, ny
-            do i = 1, nx + 1
-                ranval = rand(0)
-                u(i,j,k) = (ranval - 0.5) * uperturb
-             end do
-        end do
-     end do
-
- 
+    enddo 
   end subroutine ic
 end module initial_conditions
