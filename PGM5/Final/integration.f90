a534e9257aaeeac22e2322095b9c8f932c8d36b8
Mark Van Moer <mvanmoer@illinois.edu>
2013-10-04 17:36:33 -0500

    First commit in Final development branch ->
    (Forgot I needed to add new directories as I went, that they weren't automatically picked up.)
    -- the only difference between this and F is the config files and that I've started using
    the FSON module, which will be in the next few commits as well.

---
+++ PGM5/Final/integration.f90
@@ -0,0 +1,164 @@
+!     integration.f90
+!     Mark Van Moer, ATMS502 Fall 2011
+!     Preps 3D arrays for calls to 1D advection routine
+
+module integration
+  use mesh_type
+  implicit none
+  public
+contains
+  subroutine integrate(t1, t2, u1, v1, w1, u2, v2, w2, u3, v3, w3, & 
+       m, dt, CTdt)
+    ! t1 -- physical domain scalars at current time step
+    ! t2 -- physical domain scalars at next time step
+    ! u  -- u-component of wind field
+    ! v  -- v-component of wind field
+    ! w  -- w-component of wind field
+    ! dx, dy, dz -- grid spacing
+    ! dt -- time delta   
+    ! nx, ny, nz -- dims
+    use advection
+    type (mesh), intent(in) :: m
+    real, dimension(-1:m%nx+2,-1:m%ny+2,-1:m%nz+2), intent(inout) :: t1, t2
+    real, dimension(0:m%nx+1,0:m%ny+1,0:m%nz+1), intent(in) :: u1, v1, w1, u2, v2, w2
+    real, dimension(0:m%nx+1,0:m%ny+1,0:m%nz+1), intent(inout) :: u3, v3, w3
+    real, intent(in) :: dt, CTdt
+    integer :: i, j, k
+    real :: uu_x, vu_y, wu_z, uv_x, vv_y, wv_z, uw_x, vw_y, ww_z
+
+    uu_x = 0.0
+    vu_y = 0.0
+    wu_z = 0.0
+    uv_x = 0.0
+    vv_y = 0.0
+    wv_z = 0.0
+    uw_x = 0.0
+    vw_y = 0.0
+    ww_z = 0.0
+
+    call uadvect()
+    call vadvect()
+    call wadvect()
+    call strang()
+
+  contains
+    subroutine uadvect()
+      ! U advection  
+      !$OMP PARALLEL DO PRIVATE (i,j,k)
+      do k = 1, m%nz
+         do j = 1, m%ny
+            do i = 2, m%nx
+               uu_x = (u2(i+1,j,k)**2 - u2(i-1,j,k)**2)/(4.0*m%dx)
+       
+               vu_y = ((v2(i,j+1,k) + v2(i-1,j+1,k)) * &
+                    (u2(i,j+1,k) - u2(i,j,k)) + &
+                    (v2(i,j,k) + v2(i-1,j,k)) * &
+                    (u2(i,j,k) - u2(i,j-1,k))) / (4.0 * m%dy)
+               
+               wu_z = ((w2(i,j,k+1) + w2(i-1,j,k+1)) * &
+                    (u2(i,j,k+1) - u2(i,j,k)) + &
+                    (w2(i,j,k) + w2(i-1,j,k)) * &
+                    (u2(i,j,k) - u2(i,j,k-1))) / (4.0 * m%dz)
+               
+               u3(i,j,k) = u3(i,j,k) - CTdt * (uu_x + vu_y + wu_z)
+            end do
+         end do
+      end do
+      !$OMP END PARALLEL DO
+    end subroutine uadvect
+
+    subroutine vadvect()
+      ! V advection
+      !$OMP PARALLEL DO PRIVATE (i,j,k)
+      do k = 1, m%nz
+         do j = 1, m%ny
+            do i = 1, m%nx
+               uv_x = ((u2(i+1,j,k) + u2(i+1,j-1,k)) * &
+                    (v2(i+1,j,k) - v2(i,j,k)) + &
+                       (u2(i,j,k) + u2(i,j-1,k)) * &
+                       (v2(i,j,k) - v2(i-1,j,k))) / (4.0 * m%dx)
+              
+               vv_y = (v2(i,j+1,k)**2 - v2(i,j-1,k)**2)/(4.0*m%dy)
+               
+               ! on third glance, original was correct...
+               wv_z = ((w2(i,j,k+1) + w2(i,j-1,k+1)) * &
+                    (v2(i,j,k+1) - v2(i,j,k)) + &
+                    (w2(i,j,k) + w2(i,j-1,k)) * &
+                    (v2(i,j,k) - v2(i,j,k-1))) / (4.0 * m%dz)
+
+               v3(i,j,k) = v3(i,j,k) - CTdt * (uv_x + vv_y + wv_z)
+            end do
+         end do
+      end do
+      !$OMP END PARALLEL DO
+    end subroutine vadvect
+
+    subroutine wadvect
+      ! W advection
+      !$OMP PARALLEL DO PRIVATE (i,j,k)
+      do k = 2, m%nz
+         do j = 1, m%ny
+            do i = 1, m%nx
+               uw_x = ((u2(i+1,j,k) + u2(i+1,j,k-1)) * &
+                    (w2(i+1,j,k) - w2(i,j,k)) + &
+                    (u2(i,j,k) + u2(i,j,k-1)) * &
+                    (w2(i,j,k) - w2(i-1,j,k))) / (4.0 * m%dx)
+              
+               vw_y = ((v2(i,j+1,k) + v2(i,j+1,k-1)) * &
+                    (w2(i,j+1,k) - w2(i,j,k)) + &
+                    (v2(i,j,k) + v2(i,j,k-1)) * &
+                    (w2(i,j,k) - w2(i,j-1,k))) / (4.0 * m%dy)
+               
+               ww_z = (w2(i,j,k+1)**2 - w2(i,j,k-1)**2)/(4.0*m%dz)
+               
+               w3(i,j,k) = w3(i,j,k) - CTdt * (uw_x + vw_y + ww_z)
+            end do
+         end do
+      end do
+      !$OMP END PARALLEL DO
+
+    end subroutine wadvect
+
+
+    subroutine strang()
+      ! Strang splitting - 
+      ! Fx(dt/2),Fy(dt/2),Fz(dt),Fy(dt/2),Fx(dt/2)
+
+      ! do x dir
+      do j = 1, m%ny
+         do k = 1, m%nz
+            call advect1D(t1(:,j,k), t2(:,j,k), u1(:,j,k), m%dx, dt/2.0)
+         end do
+      end do
+
+      ! Do y dir
+      do i = 1, m%nx
+         do k = 1, m%nz
+            call advect1D(t1(i,:,k),t2(i,:,k), v1(i,:,k), m%dy, dt/2.0)
+         end do
+      end do
+
+      ! do z dir
+      do i = 1, m%nx
+         do j = 1, m%ny
+            call advect1D(t1(i,j,:), t2(i,j,:), w1(i,j,:), m%dz, dt)
+         end do
+      end do
+
+      ! do y dir
+      do i = 1, m%nx
+         do k = 1, m%nz
+            call advect1D(t1(i,:,k),t2(i,:,k), v1(i,:,k), m%dy, dt/2.0)
+         end do
+      end do
+
+      ! do x dir
+      do j = 1, m%ny
+         do k = 1, m%nz
+            call advect1D(t1(:,j,k),t2(:,j,k), u1(:,j,k), m%dx, dt/2.0)
+         end do
+      end do
+      return
+    end subroutine strang
+  end subroutine integrate
+end module integration
---
+++ PGM5/Final/pgm5.f90
@@ -0,0 +1,233 @@
+!     Program 5 - 3D nonlinear quasi-compressible flow
+!     ATMS502 Fall 2011
+!     Mark Van Moer
+!
+!     Make with supplied makefile
+!
+!  Test E
+!
+program pgm5
+  use initial_conditions
+  use boundary_conditions
+  use integration
+  use pgf_bouyancy
+  use diffusion
+  use savedata
+  use read_parameters_module 
+  use mesh_type
+  implicit none
+  type (mesh) m
+  !integer :: nx, ny, nz
+  !real :: dx, dy, dz
+  real :: dt
+
+  real :: CTdt ! dt for centered time
+ 
+  real, dimension(:,:,:), allocatable :: u1, u2, u3, v1, v2, v3, w1, w2, w3
+  real, dimension(:,:,:), allocatable :: t1, t2, tprime, t1a
+  real, dimension(:,:,:), allocatable :: p1, p2, p3
+  real, dimension(:), allocatable :: rho_t
+  real, dimension(:), allocatable :: rho_w
+
+  real :: Thetabar = 300.0
+
+  ! strings
+  character(len=*), parameter :: fmt='(I5,1X,F4.0,3X,F7.3,1X,F7.3,2X,F7.3,1X,F7.3,2X,F7.3,1X,F6.3,2X,F8.4,2X,F6.4,2X,F8.2,1X,F7.2)'
+
+  integer :: n, nstep, nplot
+  integer :: i, j, k
+
+  call read_parameters("inputfile", m, dt, nstep, nplot) 
+
+  allocate(u1(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           u2(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           u3(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           v1(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           v2(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           v3(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           w1(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           w2(0:m%nx+1,0:m%ny+1,0:m%nz+1), &
+           w3(0:m%nx+1,0:m%ny+1,0:m%nz+1))
+
+  allocate(t1(-1:m%nx+2,-1:m%ny+2,-1:m%nz+2), &
+           t2(-1:m%nx+2,-1:m%ny+2,-1:m%nz+2), &
+           tprime(-1:m%nx+2,-1:m%ny+2,-1:m%nz+2), &
+           t1a(-1:m%nx+2,-1:m%ny+2,-1:m%nz+2))
+
+  allocate(p1(0:m%nx+1,0:m%ny+1,m%nz), &
+           p2(0:m%nx+1,0:m%ny+1,m%nz), &
+           p3(0:m%nx+1,0:m%ny+1,m%nz))
+
+  allocate(rho_t(1:m%nz), rho_w(1:m%nz+1))
+
+  ! set initial conditions
+  call ic(t1(1:m%nx,1:m%ny,1:m%nz), u1(1:m%nx+1,1:m%ny,1:m%nz), v1(1:m%nx,1:m%ny+1,1:m%nz), &
+       rho_t, rho_w, m)
+
+  ! everything else is zeros
+  p1 = 0.0
+  p2 = 0.0
+  p3 = 0.0
+  u2 = 0.0
+  u3 = 0.0
+  v2 = 0.0
+  v3 = 0.0
+  w1 = 0.0
+  w2 = 0.0
+  w3 = 0.0
+
+  ! plot initial condition
+  tprime = t1 - Thetabar
+  call plotunstaggered(0)
+
+
+  print*, "Step  Time   umin    umax    vmin     vmax     wmin    wmax   tmin      tmax        pmin     pmax"
+  print*, "-------------------------------------------------------------------------------------------------"
+  write(*,fmt) 0, 0.0, minval(u1), maxval(u1), minval(v1), maxval(v1), &
+       minval(w1), maxval(w1), minval(tprime(1:m%nx,1:m%ny,1:m%nz)), &
+       maxval(tprime), minval(p1), maxval(p1)
+
+  call bc(u1, u2, u3, v1, v2, v3, w1, w2, w3, &
+       t1, t2, p1, p2, m)
+
+  CTdt = dt
+
+  !integration loop
+  do n = 1, nstep
+  
+     ! start the leapfrogging
+     t1a = t1
+     t2 = t1
+     u3 = u1
+     v3 = v1
+     w3 = w1
+     p3 = p1
+
+     ! call advect
+     ! Passes Tests A1-3 for theta advection
+     ! Passes Test D for Theta only advection
+     call integrate(t1, t2, u1, v1, w1, u2, v2, w2, &
+          u3, v3, w3, m, dt, CTdt)
+    
+     ! call diffusion
+     ! Passes Test C
+     ! t1a is unadvected t1, t2 is advected t1
+     call diff(t1a, u3, v3, w3, u1, v1, w1, &
+          m, CTdt) 
+
+     ! call PGF
+     ! Passes Test B
+     call pgf(u3, v3, w3, p3, p1, t2, rho_t, rho_w, &
+          m, CTdt)
+
+     
+
+     ! array update
+     ! -- currently doing theta update in advect1D
+     if (n .ne. 1) then
+        u1 = u2
+        v1 = v2
+        w1 = w2
+        p1 = p2
+     end if
+
+     u2 = u3
+     v2 = v3
+     w2 = w3
+     p2 = p3
+
+     ! for CT, first iteration just works like FT
+     if (n == 1) CTdt = 2.0 * dt
+
+     ! call bc for next iteration
+     call bc(u1, u2, u3, v1, v2, v3, w1, w2, w3, &
+          t1, t2, p1, p2, m)
+
+     tprime = t1 - Thetabar   
+
+     if (mod(n,nplot) == 0) then
+        call plotunstaggered(n)
+     end if
+
+     !print intermediate values every 0..9, then by tens
+     if (n < 10 .or. mod(n, 10) == 0 .or. n == nstep) then     
+        write(*,fmt) n, dt * real(n), &
+             minval(u3(1:m%nx+1,1:m%ny,1:m%nz)), maxval(u3(1:m%nx+1,1:m%ny,1:m%nz)), &
+             minval(v3(1:m%nx,1:m%ny+1,1:m%nz)), maxval(v3(1:m%nx,1:m%ny+1,1:m%nz)), &
+             minval(w3(1:m%nx,1:m%ny,1:m%nz+1)), maxval(w3(1:m%nx,1:m%ny,1:m%nz+1)), &
+             minval(tprime(1:m%nx,1:m%ny,1:m%nz)), maxval(tprime(1:m%nx,1:m%ny,1:m%nz)), &
+             minval(p3(1:m%nx,1:m%ny,1:m%nz)), maxval(p3(1:m%nx,1:m%ny,1:m%nz))
+     end if
+
+  end do ! time loop finished     
+  deallocate(u1, u2, u3, v1, v2, v3, w1, w2, w3, t1, t2, tprime, t1a, p1, p2, p3, rho_t, rho_w)
+
+contains
+  ! unstaggers u,v,w and plots along with theta prime and p
+  ! Tried OMP triple-loops here, but this was causing the crashes
+  ! Going with regular loops.
+  subroutine plotunstaggered(step)
+    integer, intent(in) :: step
+    real, dimension(1:m%nx,1:m%ny,1:m%nz) :: plotU,plotV,plotW
+
+    if (step < 1.0) then
+       ! for initial conditions, plot u1,w1,v1
+       do k = 1, m%nz
+          do j = 1, m%ny
+             do i = 1,m%nx  
+                plotW(i,j,k) = 0.5 * (w1(i,j,k+1) + w1(i,j,k))
+             end do
+          end do
+       end do
+
+       do k = 1,m%nz
+          do j = 1,m%ny
+             do i = 1,m%nx      
+                plotU(i,j,k) = 0.5 * (u1(i+1,j,k) + u1(i,j,k))
+             end do
+          end do
+       end do
+
+       do k = 1,m%nz
+          do j = 1,m%ny
+             do i = 1,m%nx
+                plotV(i,j,k) = 0.5 * (v1(i,j+1,k) + v1(i,j,k))
+             end do
+          end do
+       end do
+    else
+       ! all other steps plot u3,v3,w3
+       do k = 1, m%nz
+          do j = 1, m%ny
+             do i = 1,m%nx  
+                plotW(i,j,k) = 0.5 * (w3(i,j,k+1) + w3(i,j,k))
+             end do
+          end do
+       end do
+
+       do k = 1,m%nz
+          do j = 1,m%ny
+             do i = 1,m%nx      
+                plotU(i,j,k) = 0.5 * (u3(i+1,j,k) + u3(i,j,k))
+             end do
+          end do
+       end do
+
+       do k = 1,m%nz
+          do j = 1,m%ny
+             do i = 1,m%nx
+                plotV(i,j,k) = 0.5 * (v3(i,j+1,k) + v3(i,j,k))
+             end do
+          end do
+       end do
+    end if
+
+!   call savefield("U", step, plotU, nx, ny, nz)
+!   call savefield("V", step, plotV, nx, ny, nz)
+!   call savefield("W", step, plotW, nx, ny, nz)
+    call savefield("test", step, plotU, plotV, plotW, m)
+    call savefield("T", step, tprime(1:m%nx,1:m%ny,1:m%nz), m) 
+    call savefield("P", step, p3(1:m%nx,1:m%ny,1:m%nz), m)
+
+  end subroutine plotunstaggered
+end program pgm5
