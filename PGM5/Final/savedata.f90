3e55dcd20699884bdf6db29bb205dc642d9dd04d
Mark Van Moer <mvanmoer@illinois.edu>
2013-10-16 16:47:20 -0500

    removed out of date comments

--- PGM5/Final/savedata.f90
+++ PGM5/Final/savedata.f90
@@ -1,23 +1,3 @@
-! -------------------------------------------------------
-! PUTFIELD - write a data field to the run history file.
-! -------------------------------------------------------
-! Arguments:
-!
-!   name       input   char *    name of the field (only 4 chars used)
-!   datatime   input   real      model integration time
-!   field      input   real      array of data
-!   nx,ny,nz   input   integers  dimensions of field()
-!
-! If name = "*", the output file is closed (nothing else done)
-!
-! ==NOTE== make sure you pass a character string, e.g. "NAME"
-!          rather than just a character, even if that string
-!          is 1-char long.  A string is assumed as input.
-!
-! A header integer written to the file is set to 0 if the
-! file is created from Fortran, and 1 from C.
-!
-!
 ! mvm 20130117 -- changing from B. Jewett's
 !  -- moving to Fortran 2003 in order to use streaming access
 !  -- writing each timestep as a separate file, instead of one file containing
